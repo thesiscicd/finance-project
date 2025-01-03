3bd66f2de9e88f7b598d5da748216a892dd18ab2
thesiscicd <thesiscicd@gmail.com>
2025-01-03 17:35:32 +0545

    Update settings.py

--- FinanceTracker/settings.py
+++ FinanceTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s!ypkaq&(k1ew44f4+qy2p$i=*jh9jz0&7(pmw#q-p7r781pc-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
